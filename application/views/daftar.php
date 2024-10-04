f085e3ca43988e6ef7fbe0c8612044d4420d79d6
aghniaaulia <aulialiati@gmail.com>
2024-10-04 08:47:19 +0700

    ubah_daftar

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -1,7 +1,9 @@
 <h1>Tambah User</h1>
+<div class="tambah">
 <form action="<?= base_url() ?>daftar/simpan" method="post">
   <input type="text" name="username" placeholder="masukkan username"><br><br>
   <input type="password" name="password" placeholder="masukkan password"><br><br>
   <input type="text" name="nama" placeholder="masukkan nama lengkap"><br><br>
   <input type="submit" value="SIMPAN">
+</div>
 </form>
--- application/views/templete/header.php
+++ application/views/templete/header.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="<?php echo base_url('assets/tambah.css')?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/edit.css')?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/menu.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/daftar.css')?>">
   <title>Document</title>
 </head>
 <body>
